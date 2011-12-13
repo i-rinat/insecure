9733f766855d20cc0b5246195ed973ac645c470c
Rinat <ibragimovrinat@mail.ru>
2011-12-13 20:44:57 +0400

    + readdir, getattr, init

--- insecure.c
+++ insecure.c
@@ -22,48 +22,97 @@ struct insecure_state {
     sqlite3 *db;
 };
 
+static const char *sql_create_tables =
+    "CREATE TABLE IF NOT EXISTS fit (id INTEGER PRIMARY KEY, fname TEXT, backname TEXT, parent INTEGER); "
+    "CREATE UNIQUE INDEX IF NOT EXISTS fit_fname ON fit (fname); "
+    "INSERT OR IGNORE INTO fit (id, fname, parent, backname) VALUES (1, '/', 0, '/'); ";
+
 #define FS_DATA ((struct insecure_state *) fuse_get_context()->private_data)
 
 int insecure_getattr (const char *path, struct stat *stbuf) {
-    char *full_path;
     int ret;
+    int rc;
 
-    full_path = (char *) calloc (strlen(path) + FS_DATA->backend_point_len + 1, 1);
-    strcpy (full_path, FS_DATA->backend_point);
-    strcat (full_path, path);
+    gchar *full_path;
+    struct insecure_state *state = FS_DATA;
 
-    ret = stat (full_path, stbuf);
-    free (full_path);
+    sqlite3_stmt *stmt;
+    rc = sqlite3_prepare_v2 (state->db, "SELECT backname FROM fit WHERE fname=?;", -1, &stmt, NULL);
+    // printf ("sqlite3_prepare rc = %d\n", rc);
+    rc = sqlite3_bind_text (stmt, 1, path, -1, SQLITE_TRANSIENT);
+    // printf ("sqlite3_bind_text rc = %d\n", rc);
+    rc = sqlite3_step (stmt);
+    // printf ("sqlite3_step rc = %d\n", rc);
+
+    if (SQLITE_ROW == rc) {
+        printf ("there is a file named %s, ", path);
+        printf ("and it backed at file %s\n", sqlite3_column_text (stmt, 0));
+
+        gchar *full_path = g_build_path ("/", state->backend_point, sqlite3_column_text (stmt, 0), NULL);
+        ret = stat (full_path, stbuf);
+        g_free (full_path);
+    } else {
+        printf ("there is no file named %s\n", path);
+        ret = -1;
+        errno = ENOENT;
+    }
+
+    sqlite3_finalize (stmt);
 
     return ret;
 }
 
 void *insecure_init (struct fuse_conn_info *conn) {
     printf("init\n");
+
+    struct insecure_state *state = FS_DATA;
+    int rc;
+
+    gchar *db_name = g_build_path ("/", state->backend_point, ".filenames.db", NULL);
+    rc = sqlite3_open (db_name, &state->db);
+    printf ("db_name = %s\n", db_name);
+    printf ("rc = %d\n", rc);
+    // FIXME: error handling
+    rc = sqlite3_exec (state->db, sql_create_tables, NULL, 0, NULL);
+    printf ("rc = %d\n", rc);
+
     return FS_DATA;
 }
 
 int insecure_readdir (const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
 {
     printf ("readdir on %s\n", path);
+    struct insecure_state *state = FS_DATA;
+    int rc;
 
-    gchar *full_path = g_build_path ("/", FS_DATA->backend_point, path, NULL);
-    struct dirent *de;
-    DIR *dp;
-    dp = opendir (full_path);
-    g_free (full_path);
+    sqlite3_stmt *stmt;
 
-    while (NULL != (de = readdir (dp))) {
-        if (0 != filler (buf, de->d_name, NULL, 0)) {
+    rc = sqlite3_prepare_v2 (state->db,
+        "SELECT fit.fname FROM fit, fit as p WHERE p.fname=? AND p.id=fit.parent",
+        -1,     // nByte
+        &stmt,
+        NULL    // pzTail
+    );
+
+    rc = sqlite3_bind_text (stmt, 1, path, -1, SQLITE_TRANSIENT);
+
+    filler (buf, ".", NULL, 0);
+    filler (buf, "..", NULL, 0);
+
+    while (SQLITE_ROW == (rc = sqlite3_step (stmt))) {
+        if (0 != filler (buf, (char *)sqlite3_column_text (stmt, 0), NULL, 0)) {
+            sqlite3_finalize (stmt);
             return -ENOMEM;
         }
     }
+    sqlite3_finalize (stmt);
 
     return 0;
 }
 
 int insecure_open (const char *path, struct fuse_file_info *fi) {
     printf ("open\n");
+    /*
     gchar *full_path = g_build_path ("/", FS_DATA->backend_point, path, NULL);
     int fd = open (full_path, fi->flags);
     if (fd < 0) {
@@ -71,6 +120,9 @@ int insecure_open (const char *path, struct fuse_file_info *fi) {
     }
     fi->fh = (unsigned int) fd;
     printf ("handle %d\n", fd);
+    */
+
+
     return 0;
 }
 
@@ -85,8 +137,6 @@ int insecure_read (const char *path, char *buf, size_t size, off_t offset, struc
 
     return bytes_read;
 }
-
-
 
 struct fuse_operations insecure_op = {
     .getattr = insecure_getattr,
